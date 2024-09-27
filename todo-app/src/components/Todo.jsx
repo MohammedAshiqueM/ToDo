5e3b4987653341751f3c757495d8e83bbad0b478
moh-ashique <mohammedashique942001@gmail.com>
2024-09-28 01:32:39 +0530

    local storage

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,16 +1,34 @@
-import { useState } from "react"
-import Form from "./form"
-import List from "./list"
-import Head from "./head"
-export default function Todo(){
-    const [todos,setToDos] = useState([])
-    const cnt = todos.filter((todo) => todo.done).length
-    const total = todos.length
-    return(
+import { useState, useEffect } from "react";
+import Form from "./form";
+import List from "./list";
+import Head from "./head";
+
+export default function Todo() {
+    const [todos, setToDos] = useState([]);
+    useEffect(() => {
+        if (todos.length === 0) {
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }, [todos]);
+
+    useEffect(() => {
+        const storedTodos = localStorage.getItem("todos");
+        if (storedTodos) {
+            setToDos(JSON.parse(storedTodos));
+        }
+    }, []);
+
+
+    const cnt = todos.filter((todo) => todo.done).length;
+    const total = todos.length;
+
+    return (
         <>
-        <Head cnt={cnt} cmplt={total}/>
-        <Form todos={todos} setToDos={setToDos}/>
-        <List todos={todos} setToDos={setToDos}/>
+            <Head cnt={cnt} cmplt={total} />
+            <Form todos={todos} setToDos={setToDos} />
+            <List todos={todos} setToDos={setToDos} />
         </>
-    )
+    );
 }
